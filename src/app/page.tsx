7fc504eca687f599ebe4fb61c835432fcb65d5ae
Muhammad Ghazi <mghazidev@gmail.com>
2024-12-27 21:40:34 +0500

    theme configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import { TInputField } from "@/components/ui/input/TInputField";
 
 export default function Home() {
   return (
-    <div className="w-48 m-auto">
-      <TInputField />
+    <div className="w-48 m-auto prose">
+      <h2 className="h1">Heading 2</h2>
+      <p className="body-1-medium">Medium Body Text</p>
+
+      <TInputField className="border p-2 rounded-md" />
     </div>
   );
 }
