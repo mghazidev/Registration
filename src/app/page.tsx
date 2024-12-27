6e956b981fc9e701ffa9d885ae6a2525f3022af0
Muhammad Ghazi <mghazidev@gmail.com>
2024-12-27 20:45:16 +0500

    theme configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,6 @@ import { TInputField } from "@/components/ui/input/TInputField";
 export default function Home() {
   return (
     <div className="w-48 m-auto">
-      <h1 className="text-10 font-bold font-customFustat leading-24 tracking-wide">
-        hellow
-      </h1>
-
       <TInputField />
     </div>
   );
