9b8323b2ddb536713d316c138f514500e044b142
Muhammad Ghazi <mghazidev@gmail.com>
2024-12-30 20:52:18 +0500

    verification input field added

--- src/components/ui/input/TInputField.tsx
+++ src/components/ui/input/TInputField.tsx
@@ -1,21 +1,35 @@
 import * as React from "react";
-
+import WarningIcon from "@/svgs/WarningIcon";
 import { cn } from "@/lib/utils";
 
 const TInputField = React.forwardRef<
   HTMLInputElement,
-  React.ComponentProps<"input">
->(({ className, type, ...props }, ref) => {
+  React.ComponentProps<"input"> & {
+    error?: boolean;
+    errorMessage?: string;
+  }
+>(({ className, type, error, errorMessage, ...props }, ref) => {
   return (
-    <input
-      type={type}
-      className={cn(
-        "flex w-full font-customFustat bg-primary-white body-1-medium h-11 rounded-m border border-functional-500 px-5 py-6 text-functional-1000 shadow-sm transition-colors placeholder:text-functional-500 focus-visible:border-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-brand-50 disabled:cursor-not-allowed disabled:opacity-50 ",
-        className
+    <div className="space-y-1">
+      <input
+        type={type}
+        className={cn(
+          "flex w-[100%] font-customFustat bg-primary-white body-1-medium h-11 rounded-m border  px-5 py-6 text-functional-1000 shadow-sm transition-colors placeholder:text-functional-500 focus-visible:border-none focus-visible:outline-none focus-visible:ring-2  disabled:cursor-not-allowed disabled:opacity-50 ",
+          error
+            ? "border-red-800 focus-visible:ring-red-800"
+            : "border-functional-500 focus-visible:ring-brand-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+      {error && errorMessage && (
+        <div className="flex items-center gap-2">
+          <WarningIcon className="h-5 w-5 text-red-800" />
+          <p className="text-red-800 body-3-regular">{errorMessage}</p>
+        </div>
       )}
-      ref={ref}
-      {...props}
-    />
+    </div>
   );
 });
 TInputField.displayName = "TInputField";
--- src/components/ui/input/TVerificationInputField.tsx
+++ src/components/ui/input/TVerificationInputField.tsx
@@ -3,7 +3,6 @@ import { TInputField } from "../input/TInputField";
 import CheckIcon from "@/svgs/CheckIcon";
 import RefreshIcon from "@/svgs/RefreshIcon";
 import CrossCheckIcon from "@/svgs/CrossCheckIcon";
-import WarningIcon from "@/svgs/WarningIcon";
 
 interface VerificationInputFieldProps {
   id: string;
@@ -33,7 +32,7 @@ const VerificationInputField: React.FC<VerificationInputFieldProps> = ({
       case "not_verified":
         return <CrossCheckIcon className="h-7 w-7 text-red-800" />;
       case "loading":
-        return <RefreshIcon className="h-7 w-7 animate-spin text-yellow-500" />;
+        return <RefreshIcon className="h-7 w-7 animate-spin text-yellow-300" />;
       default:
         return null;
     }
@@ -56,15 +55,13 @@ const VerificationInputField: React.FC<VerificationInputFieldProps> = ({
   };
 
   return (
-    <div className="w-full space-y-2">
-      <div className="relative flex items-center">
+    <div className="w-full space-y-4">
+      <div className="relative ">
         <TInputField
           id={id}
-          className={`flex-1 pr-22 focus-visible:ring-1 border-none ring-1 shadow-none ${
-            error
-              ? "ring-red-800 focus-visible:ring-red-800"
-              : "ring-functional-500 focus-visible:ring-functional-500"
-          }`}
+          error={error}
+          errorMessage="Please enter a valid phone number."
+          className="flex-1 pr-22 focus-visible:ring-functional-500"
           type="text"
           placeholder={placeholder || "Enter phone number"}
           value={value}
@@ -73,7 +70,7 @@ const VerificationInputField: React.FC<VerificationInputFieldProps> = ({
         />
 
         <div
-          className={`absolute right-3 cursor-pointer flex items-center space-x-1 p-3 px-4 rounded-3xl ${
+          className={`absolute right-4 top-3 flex items-center cursor-pointer flex space-x-1 p-3 px-4 rounded-3xl ${
             verificationStatus === "verified"
               ? "bg-green-100"
               : verificationStatus === "not_verified"
@@ -88,15 +85,6 @@ const VerificationInputField: React.FC<VerificationInputFieldProps> = ({
           {getStatusText()}
         </div>
       </div>
-
-      {error && verificationStatus !== "idle" && (
-        <div className="flex gap-2">
-          <WarningIcon className="h-7 w-7 text-red-800" />
-          <p className="text-red-800 body-3-regular mt-1">
-            Please enter a valid phone number.
-          </p>
-        </div>
-      )}
     </div>
   );
 };
