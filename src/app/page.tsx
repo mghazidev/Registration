e7007f6a2211c1e2b8015cbd5fccc22b6bc6be45
Muhammad Ghazi <mghazidev@gmail.com>
2024-12-30 14:30:18 +0500

    Radio Card component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import * as React from "react";
 import { RadioButton } from "@/components/ui/radio/TRadioButton";
 import {
   Select,
@@ -6,12 +9,41 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select/TSelect";
+import { RadioCard } from "@/components/ui/radio_card/TRadioCard";
 
 export default function Home() {
+  const [selected, setSelected] = React.useState<string>("option1");
+
+  const handleChange = (value: string) => {
+    setSelected(value);
+  };
   return (
     <div className="w-[60%] m-auto ">
       <RadioButton id="option1" name="example-group" label="Yes" />
-      <Select>
+      <RadioCard
+        id="option1"
+        label="Option 1"
+        description="hehehrehie"
+        value="option1"
+        checked={selected === "option1"}
+        onChange={handleChange}
+      />
+      <RadioCard
+        id="option2"
+        label="Option 2"
+        value="option2"
+        checked={selected === "option2"}
+        onChange={handleChange}
+      />
+      <RadioCard
+        id="option3"
+        label="Option 3 (Disabled)"
+        value="option3"
+        checked={selected === "option3"}
+        onChange={handleChange}
+        disabled
+      />
+      {/* <Select>
         <SelectTrigger className="w-[180px] my-4">
           <SelectValue
             placeholder="Write for search..."
@@ -24,7 +56,7 @@ export default function Home() {
           <SelectItem value="system">System</SelectItem>
           <SelectItem value="rr">R</SelectItem>
         </SelectContent>
-      </Select>
+      </Select> */}
     </div>
   );
 }
---
+++ src/components/ui/radio_card/TRadioCard.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+import { RadioButton } from "../radio/TRadioButton";
+
+interface RadioCardProps {
+  id: string;
+  label: string;
+  description?: string;
+  error?: boolean;
+  value: string;
+  checked: boolean;
+  disabled?: boolean;
+  onChange: (value: string) => void;
+}
+
+const RadioCard: React.FC<RadioCardProps> = ({
+  id,
+  label,
+  description,
+  error,
+  value,
+  checked,
+  disabled = false,
+  onChange,
+}) => {
+  return (
+    <div
+      className={cn(
+        "flex items-center justify-between border rounded-m p-4 px-6 gap-4 cursor-pointer my-5",
+        checked
+          ? "border-brand-50 border-2"
+          : error
+          ? "border-red-800 border-2"
+          : "border-functional-500",
+        disabled && "cursor-not-allowed opacity-50"
+      )}
+      onClick={() => !disabled && onChange(value)}
+    >
+      <div>
+        <p
+          className={cn(
+            "body-1-medium ml-3",
+            checked ? "text-functional-100" : "text-functional-500",
+            error && "text-red-800"
+          )}
+        >
+          {label}
+        </p>
+        {description && (
+          <p className="body-3-regular ml-3 text-functional-500">
+            {description}
+          </p>
+        )}
+      </div>
+      <div>
+        <RadioButton
+          id={`${id}-radio`}
+          label=""
+          checked={checked}
+          disabled={disabled}
+          error={error}
+          className="peer-checked:ring-2 peer-checked:ring-brand-500"
+          onChange={() => !disabled && onChange(value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export { RadioCard };
