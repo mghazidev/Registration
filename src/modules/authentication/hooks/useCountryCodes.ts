9b0e5a2cc81fe53b906b4227975b6a87eafd9b24
Muhammad Ghazi <mghazidev@gmail.com>
2025-01-01 02:53:40 +0500

    phone input component added

--- src/modules/authentication/hooks/useCountryCodes.ts
+++ src/modules/authentication/hooks/useCountryCodes.ts
@@ -4,9 +4,9 @@ import { CountryCode } from "../types/phoneInput";
 
 export const useCountryCodes = () => {
   const { data, isLoading, error } = useQuery<CountryCode[], Error>({
-    queryKey: ["countryCodes"], // Unique key for the query
-    queryFn: fetchCountryCodes, // Function to fetch data
-    staleTime: Infinity, // Cache the data indefinitely
+    queryKey: ["countryCodes"],
+    queryFn: fetchCountryCodes,
+    staleTime: Infinity,
   });
 
   return {
